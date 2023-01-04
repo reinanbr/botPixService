f14e9d97cf41b435230f9e961a02122dea1b84af
reinanbr <perseu912@gmail.com>
2023-01-04 10:51:15 -0300

    added send gif and png bot telegram

--- bot.py
+++ bot.py
@@ -2,17 +2,15 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 from telegram import ForceReply, Update
 from os import getenv
-import kitano as kt
 from dotenv import load_dotenv
 import logging
 
+from kitano import puts
+import dreams.spankbang as sb
 
 load_dotenv()
 
-p = kt.Puts()
-puts = p.puts
-
-token = getenv('TELEGRAM_BOT_TOKEN')
+token = getenv('TOKEN_TELEGRAM_BOT')
 
 
 logging.basicConfig(
@@ -49,20 +47,40 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
 
-
-
+async def spankbang(update:Update,context:ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.message.text.replace('/spankbang','')
+    videos = sb.search_porn(query,page_limit=4)
+    size = len(videos['videos'])
+    for i,video in enumerate(videos['videos']):
+        #print(video)
+        i = i+1
+        title = video['title']
+        time = video['time']
+        url = video['url']
+        stats = video['stats'].replace('\n',' | ')
+        caption = f'[{i}/{size}]\ntitle: {title}\ntime: {time}\nurl: {url}\nstats{stats}'
+        try:
+            await update.message.reply_animation(video['gif'],caption=caption)
+        except:
+            puts('[{i}] error spankbang: dont send image gif')
+            try:
+                await update.message.reply_photo(video['utl_img'],caption=caption)
+            except:
+                puts(f'[{i}] error spankbang: dont send image png')
+                pass
 
 def main():
-	app = Application.builder().token(token).build()
+    app = Application.builder().token(token).build()
 
-	app.add_handler(CommandHandler("start", start))
-	app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("spankbang", spankbang))
 
 
-	app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 #	app.add_handler(CommandHandler("start", hello))
-	puts('initializing app...')
-	app.run_polling()
+    puts('initializing app...')
+    app.run_polling()
 
 
 
